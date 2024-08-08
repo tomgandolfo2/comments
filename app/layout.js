c8a424391091f53fb20b4434913f303847ca0eba
Tom Gandolfo <tomgandolfo2@gmail.com>
2024-08-08 16:28:52 -0300

    ad head tag added

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5480715870452313"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
